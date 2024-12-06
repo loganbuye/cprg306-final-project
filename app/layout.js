526819104e4945fd73f74d92a95633fd97b9a955
logan-buye <loganb10@learn.cssd.ab.ca>
2024-12-05 20:45:34 -0700

    Created Main Login Page and Logout function
    Created the login function and logout function on the main page.js page using google firebase

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,13 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import { AuthContextProvider } from "./_utils/auth-context";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
@@ -23,7 +25,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AuthContextProvider>{children}</AuthContextProvider>
       </body>
     </html>
   );
